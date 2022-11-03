2bf6def92e7bc96a8f9e70b2fbd078ce7c31224b
shaslhofer <haslhofer@unisoftwareplus.com>
2022-11-03 17:04:05 +0100

    add picture for dsv1_1

--- dsv1_1.m
+++ dsv1_1.m
@@ -19,43 +19,79 @@ function ret = fourier_series (a, b, f0, t)
   endfor
 end
 
-# function that calculates b vector for "Sägezahnsignal"
-function ret = coeff (len)
-  for k = 1:len
-    ret(k) = (-1)^(k-1)/k;
-  endfor
-end
-
 # calculate input parameters
 f0 = 10;
-t = [0 : 0.001 : 2/f0];
+t = [0 : 0.0001 : 2/f0];
 a = zeros(length(t),1); # vector a consists of zeros
 b = coeff(length(t));
 
 n10 = fourier_series(a, b, 10 * f0, t);
 
-#n100 = fourier_series(a, b, 100 * f0, t);
+n100 = fourier_series(a, b, 100 * f0, t);
 
-#n10000 = fourier_series(a, b, 10000 * f0, t);
+n10000 = fourier_series(a, b, 10000 * f0, t);
 
 # plot fourier series
 figure (1);
+set(gca, "fontsize", 32);
 hold on
 plot(t, n10);
 hold on
-#plot(t, n100)
+plot(t, n100)
 hold on
-#plot(t, n10000, 'g')
+plot(t, n10000, 'g')
 
 xlabel('t');
 ylabel('x(t)');
 legend('N10', 'N100', 'N10000')
 hold off
 
-# calculate and plot fourier coefficients
-n = fourier_series(a, b, 10 * f0, t);
+# plot coefficients
+figure (2);
+set(gca, "fontsize", 32);
+hold on
+bar([1:length(b)], b);
+xlabel('k');
+ylabel('bk');
+hold off
 
-function a = fourier_coeff_a(n, f0, t)
+figure (3);
+set(gca, "fontsize", 32);
+hold on
+bar([1:length(a)], a);
+xlabel('k');
+ylabel('ak');
+hold off
+
+
+
+# calculation of ck
+function ck = get_ck()
+  t0 = 1/f0;
+  prefix = 2/t0;
+
+  for k = 1:length(t)
+    func = @(t) n(k).*cos(2.*pi.*k.*f0.*t);
+    a(k) = prefix * integral(func, -t0/2, t0/2);
+  endfor
+
+  x = prefix * sum;
+end
+
+# calculation for coefficients (only for tests)
+function x = signal(t)
+  omega = 2 * pi * 10;
+  prefix = 2/pi;
+  sum = 0;
+
+  for k = 1:length(t)
+    sum += (-1)^(k-1)/k*sin(omega*k*t);
+  endfor
+
+  x = prefix * sum;
+end
+
+function a = fourier_coeff_a(f0, t)
   t0 = 1/f0;
   prefix = 2/t0;
 
@@ -65,26 +101,17 @@ function a = fourier_coeff_a(n, f0, t)
   endfor
 end
 
-function a = fourier_coeff_b(n, f0, t)
+function a = fourier_coeff_b(f0, t)
   t0 = 1/f0;
   prefix = 2/t0;
 
   for k = 1:length(t)
-    func = @(t) n(k).*sin(2.*pi.*k.*f0.*t);
-    a(k) = prefix * quad(func, -t0/2, t0/2);
+    # n(k) is wrong we need n(t)
+    func = @(t) signal(t).*sin(2.*pi.*k.*f0.*t);
+    a(k) = prefix * integral(func, -t0/2, t0/2);
   endfor
 end
 
-n10_b = fourier_coeff_b(n, f0, t);
+#n10_b = fourier_coeff_b(10 * f0, t);
 
-figure (2);
-bar([0:length(n10_b)-1], n10_b);
-#bar([0:length(b)-1], b);
-xlabel('k');
-ylabel('bk');
 
-figure (3);
-#bar([0:length(n10_a)-1], n10_a);
-bar([0:length(a)-1], a);
-xlabel('k');
-ylabel('ak');
